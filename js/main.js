e423722623389b6bdd8c78c963a6c23a7d09f99b
Brian Fuentes <Brian.Fuentes04@gmail.com>
2022-05-10 23:55:08 -0700

    made pokemon name appear after searching

--- js/main.js
+++ js/main.js
@@ -5,11 +5,10 @@ document.querySelector('#random').addEventListener('click', getRandomButton)
 function getFetch(){
   const choice = document.querySelector('input').value
   const url = `https://pokeapi.co/api/v2/pokemon/${choice}`
-  
+  document.querySelector('h3').innerText = choice
   fetch(url)
   .then(res => res.json()) // parse response as JSON
   .then(data => {
-    //console.log(data.sprites.front_default)
     document.querySelector('#pokemonSearch').src = data.sprites.front_default
     document.querySelector('.hp-stat').innerText = data.stats[0].base_stat
     document.querySelector('.attack-stat').innerText = data.stats[1].base_stat
@@ -18,7 +17,6 @@ function getFetch(){
     document.querySelector('.spDef-stat').innerText = data.stats[4].base_stat
     document.querySelector('.speed-stat').innerText = data.stats[5].base_stat
     document.querySelector('.total').innerText = data.stats.reduce((sum, current) => sum + current.base_stat, 0)
-    
   })
   .catch(err => {
       console.log(`error ${err}`)
